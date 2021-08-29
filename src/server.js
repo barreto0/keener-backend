16a5f32166bb296d63c99136d622ad9445602690
barreto <antonio909790@yahoo.com.br>
2021-08-29 15:08:21 -0300

    cors fix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 require('./database');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 const port = 3000 || process.env.PORT;
